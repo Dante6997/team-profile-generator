a50cf75071cfbf705bd27ecebf700b5a2bccacd8
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-10-23 10:11:12 -0400

    added failing employee tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,17 @@
+const { default: test } = require('node:test');
+const Employee = require('../lib/Employee');
+const employee = new Employee('Dante', 1, 'dante@yahoo.com');
+
+
+test('creates employee object', () => {
+    expect(employee.name).toBe('Dante');
+    expect(employee.id).toEqual(1);
+    expect(employee.email).toBe('dante@yahoo.com');
+});
+
+test('gets name, id, email, and role from get methods', () => {
+    expect(employee.getName()).toBe('Dante');
+    expect(employee.getId()).toBe('1');
+    expect(employee.getEmail()).toBe('dante@yahoo.com');
+    expect(employee.getRole()).toBe('Employee');
+});
