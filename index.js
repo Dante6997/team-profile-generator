6ba4dbd49663a9e42cb1b36d424cb0354a433e24
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-10-25 23:45:45 -0400

    fixed bug with questions array

--- index.js
+++ index.js
@@ -6,28 +6,31 @@ const Manager = require('./lib/Manager');
 const generateMarkdown = require('./src/generateMarkdown.js');
 const teamBuild = [];
 
-const managerPrompt = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter manager name',
-    },
+const managerPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter manager name',
+        },
+    
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter ID',
+        },
+    
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter email address',
+        },
+    
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Please enter office number',
+        },
+    ])
 
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Please enter ID',
-    },
-
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter email address',
-    },
-
-    (
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Please enter office number',
-    ),
-];
+};
